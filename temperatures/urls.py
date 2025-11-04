d54156b16a6b2d04d9681b9df82377ce86e8b3bb
josefzea <josefzea12@gmail.com>
2025-11-03 19:14:05 -0500

    Delete temperatures directory

--- temperatures/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import CityTemperatureViewSet
-
-router = DefaultRouter()
-router.register(r'temperatures', CityTemperatureViewSet, basename='citytemperature')
-
-urlpatterns = router.urls
--- temperatures/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets, permissions
-from .models import CityTemperature
-from .serializers import CityTemperatureSerializer
-
-class CityTemperatureViewSet(viewsets.ModelViewSet):
-    queryset = CityTemperature.objects.all().order_by('-last_updated')
-    serializer_class = CityTemperatureSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
